b01ac6586b1016995baafb9f8a2403ba20cd1279
AsherNoble <asherthenoble@gmail.com>
2025-10-21 18:06:26 +1100

    feat: swap logic

--- ManagerDashboardView.py
+++ ManagerDashboardView.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from SwapView import SwapView
 from TkUtils import TkUtils as ut
 
 class ManagerDashboardView:
@@ -10,7 +11,8 @@ class ManagerDashboardView:
         self.manager = self.model.get_logged_in_manager()
         self.team = self.manager.get_team()
         
-        team_name = "Your Team" if self.team is None else self.team.get_team_name()
+        self.has_team = False if self.team is None else True
+        team_name = "No team" if self.team is None else self.team.get_team_name()
         jersey_image = "none" if self.team is None else self.team.get_team_name().lower()
         
         ut.image(self.root, "image/banner.png").pack()
@@ -24,15 +26,27 @@ class ManagerDashboardView:
 
         mid = Frame(self.root)
         mid.pack(pady=(4, 8))
-        ut.button(mid, "Withdraw", self.on_withdraw).pack(side=LEFT, padx=4)
+        
+        self.withdraw_btn = ut.button(mid, "Withdraw", self.on_withdraw)
+        self.withdraw_btn.config(state=DISABLED if not self.has_team else NORMAL)
+        self.withdraw_btn.pack(side=LEFT, padx=4)
+        
         # ut.button(mid, "Manage", self.on_manage).pack(side=LEFT, padx=4)
 
         bottom = Frame(self.root)
         bottom.pack(fill=X, pady=(10, 0))
-        # ut.button(bottom, "Swap Team", self.on_swap).pack(side=LEFT, expand=True, fill=X)
+        ut.button(bottom, "Swap Team", self.on_swap).pack(side=LEFT, expand=True, fill=X)
         ut.button(bottom, "Close", self.root.destroy).pack(side=LEFT, expand=True, fill=X)
         
     def on_withdraw(self):
         self.model.withdraw_manager_from_team(self.manager)
+        self.refresh_dashboard()
+    
+    def on_swap(self):
+        win = ut.top_level("Swap")
+        SwapView(win, self.model, self.refresh_dashboard).control()
+        win.transient(self.root); win.grab_set(); win.focus_set()
+        
+    def refresh_dashboard(self):
         ut.same_window("Manager Dashboard", self.root)
         ManagerDashboardView(self.root, self.model).control()
